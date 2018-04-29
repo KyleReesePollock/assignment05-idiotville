65b74b1f4f9572818eedd91f159fc5793ff921c9
jhirs10 <jhirs10@u.rochester.edu>
2018-04-29 18:20:01 -0400

    the login is not on bootstrap anymore

--- login.php
+++ login.php
@@ -81,7 +81,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Login</title>
     
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
 
         body{ font: 14px sans-serif; }
